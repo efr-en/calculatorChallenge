3aec60fc6e428007801a50fb1bb74f6165eac73c
efr-en <efrenlaris@gmail.com>
2024-11-08 20:29:16 -0500

    began functionality of calculator and adjusted css for better aesthetic

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 let displayValue = "";
 let currentOperator = "";
 
-function updateDisplay() {
+function updateDisplay(value) {
     const display = document.getElementById('display');
-    display.innerHTML = "";
+    display.textContent = value;
 }
+// console.log(updateDisplay())
 
 function clearDisplay() {
     displayValue = "";
     currentOperator = "";
+    updateDisplay("0")
+}
+
+function appendNumber(number) {
+    if (displayValue === 0 && number !== ".") {
+        displayValue = number;
+    } else {
+        displayValue += number;
+    }
+    updateDisplay(displayValue);
 }
 
 // need functions for clearing display, updating display, calculations
